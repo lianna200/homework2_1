6d1ed72503cf62e0c1844d61f169fd1a37223bc3
Anna Litvinenko <al224@afsconnect2.njit.edu>
2017-06-21 22:26:48 -0400

    changes

--- display_discount.php
+++ display_discount.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html>
+
+<?php
+//get product description, list price and discount array
+
+       $product_description = filter_input(INPUT_POST, 'product_description');
+       $list_price = filter_input(INPUT_POST,'list_price');
+       $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+ // calculate the discount
+     $discount = $list_price * $discount_percent * .01;
+         $discount_price = $list_price - $discount;
+
+// apply currency formatting to the dollar and percent amounts
+    $list_price_formatted = "$".number_format($list_price, 2);
+        $discount_percent_formatted = $discount_percent."%";
+	$discount_formatted = "$".number_format($discount, 2);
+$discount_price_formatted = "$".number_format($discount_price,2); 
+   // escape the unformatted input
+       $product_description_escaped = htmlspecialchars($product_description);
+
+?>
+
+<!DOCTYPE thml>
+
 <html>
 <head>
     <title>Product Discount Calculator</title>
@@ -6,22 +28,24 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Product Discount Calculator</h1>
 
-        <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+<label>Product Description:</label>
+        <span><b><?php echo $product_description_escaped; ?></b></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price_formatted; ?></span><br>
 
-        <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <label>Discount Percent:</label>
+        <span><?php echo $discount_percent_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
-    </main>
+  
+ <span><i><?php echo $discount_price_formatted; ?></i></span><br>
+
+</main>
 </body>
 </html>
